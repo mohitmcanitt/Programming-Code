37aceb38e8ff279b86496d694d312048c808f2a0
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2021-07-13 01:57:24 +0530

    Update 08. Check any subset sum is equal to target.cpp

--- DP/08. Check any subset sum is equal to target.cpp
+++ DP/08. Check any subset sum is equal to target.cpp
@@ -1,29 +1,43 @@
-bool isSubsetSum(int N, int arr[], int sum){
-        // code here 
-        bool dp[N+1][sum+1];
-        for(int i=0;i<=N;i++)
+#include<bits/stdc++.h>
+using namespace std;
+
+bool solve(vector<int>v,int target)
+{
+    int n=v.size();
+    bool dp[n+1][target+1];
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=target;j++)
         {
-            for(int j=0;j<=sum;j++)
+            if(i==0 and j==0)
+                dp[i][j]=true;
+            else if(i==0)   
+                dp[i][j]=false;
+            else if(j==0) 
+                dp[i][j]=true;
+            else
             {
-                if(i==0 and j==0)
-                    dp[i][j]=true;
-                else if(i==0)
-                    dp[i][j]=false;
-                else if(j==0)
+               if(dp[i-1][j]==true)
                     dp[i][j]=true;
                 else
                 {
-                    bool case1=dp[i-1][j]; // khud run nhi bnayega
-                    bool case2=false;    // khud run bnayega
-                    if(arr[i-1]<=j)
-                    {
-                        if(dp[i-1][j-arr[i-1]])
-                            case2=true;
-                    }
-                    dp[i][j]=case1 or case2;
-                }
-                    
-            }
+                    if(v[i-1]>j)
+                        dp[i][j]=false;
+                    else
+                        dp[i][j]=dp[i-1][j-v[i-1]];    
+                }    
+            }    
         }
-        return dp[N][sum];
     }
+  return dp[n][target];
+}
+
+
+int main() {
+    vector<int>v={3, 34, 4, 12, 5, 2};
+    if(solve(v,9))
+        cout<<"\nPresent";
+        else
+        cout<<"\nAbsent";
+}
