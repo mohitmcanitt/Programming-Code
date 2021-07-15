1bd466e887af6e01d625fb6144bf61bf75984dba
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2021-07-15 18:27:45 +0530

    Update 22. Print Longest Substring without DP.cpp

--- DP/22. Print Longest Substring without DP.cpp
+++ DP/22. Print Longest Substring without DP.cpp
@@ -1,6 +1,17 @@
 
 // Leetcode 5
 // video link : https://www.youtube.com/watch?v=jCOJk4UyO8w
+//https://www.geeksforgeeks.org/longest-palindromic-substring-set-2/
+
+The idea is to generate all even length and odd length palindromes and keep track of the longest palindrome seen so far.
+To generate odd length palindrome, Fix a centre and expand in both directions for longer palindromes,
+i.e. fix i (index) as center and two indices as i1 = i+1 and i2 = i-1
+Compare i1 and i2 if equal then decrease i2 and increase i1 and find the maximum length. 
+Use a similar technique to find the even length palindrome.
+Take two indices i1 = i and i2 = i-1 and compare characters at i1 and i2 and 
+find the maximum length till all pair of compared characters are equal and store the maximum length.
+Print the maximum length.
+
 string longestPalindrome(string s) {
         int start=0;
         int end=0;
