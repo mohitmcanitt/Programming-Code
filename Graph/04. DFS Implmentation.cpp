8fcf60be3ec1f33080adf05bfdfb50a6d3e0f4e9
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2021-07-15 20:06:46 +0530

    Update 04. DFS Implmentation.cpp

--- Graph/04. DFS Implmentation.cpp
+++ Graph/04. DFS Implmentation.cpp
@@ -1,20 +1,26 @@
 /*
-BFS------> Breadth First 
-Steps:
-1. Take stack and a set
-2. stack will store the vertex
-3. set will store visited vertex
-4. put source vertex in set and stack
-5. Pop from stack
-6. Print top element
-7. Push its child which is not visited in stueue and set
-7. repeat 5-7 untill stack is not empty
-
 Input:
+
+A B 
+A S 
+S C 
+C D 
+S G 
+G F 
+C E 
+C F 
+E H 
+G H
+
+DFS, If source is C
+OUTPUT:
+C S A B G F H E D 
+
+
 Graph example taken from below link
 https://www.youtube.com/watch?v=stRst6p9s8NVg
 
-Output:
+Adjlist :
 
 A-->B,S,
 B-->A,
@@ -25,79 +31,47 @@ F-->G,C,
 G-->S,F,H,
 H-->E,G,
 S-->A,C,G,
-DFS, If source is C
-C S A B G F H E D 
+
+
+
 */
 
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Graph{
-    int V;
-    map<char,vector<char>>m;
-
-    public:
-    Graph(int v)
-    {
-        V=v;
-    }
-
-    void addEdge(char u,char v)
-    {
-        m[u].push_back(v);
-        m[v].push_back(u);
-    }
-    
-    void printEdge()
-    {
-        for(auto vertex:m)
-            {
-                cout<<vertex.first<<"-->";
-                for(auto nbhrs:vertex.second)
-                cout<<nbhrs<<",";
-                cout<<endl;
-            }
-    }
-
-    void dfs_helper(char src,map<char,bool>&visited)
-    {
-        cout<<src<<" ";
-        visited[src]=true;
-
-        for(auto node:m[src])
-            {
-                if(!visited[node])
-                dfs_helper(node,visited);
-            }
-    }
-
-    void dfs_recursive(char src)
-    {
-        map<char,bool>visited;
-
-        for(auto x:m)   // Make each vertex as non visited
-        visited[x.first]=false;
-
-        dfs_helper(src,visited);
-
-    }
-};
-int main()
+void dfs_helper(map<char,vector<char>>adjList, char src, map<char,bool>&visited)
 {
-    Graph g(10);
-    g.addEdge('A','B');
-    g.addEdge('A','S');
-    g.addEdge('S','C');
-    g.addEdge('C','D');
-    g.addEdge('S','G');
-    g.addEdge('G','F');
-    g.addEdge('C','E');
-    g.addEdge('C','F');
-    g.addEdge('E','H');
-    g.addEdge('G','H');
-    g.printEdge();
-
-    g.dfs_recursive('C');
+	visited[src]=true;
+	cout<<src<<" ";
+	for(auto x:adjList[src])
+		{
+			if(visited[x]==false)
+				dfs_helper(adjList,x,visited);
+		}
 
 }
+
+void dfs(map<char,vector<char>>adjList,char src)
+{
+	map<char,bool>visited;
+	for(auto x:adjList)
+		visited[x.first]=false;
+	dfs_helper(adjList,src,visited);	
+}
+
+int main() {
+
+    map<char,vector<char>>adjList;
+    int no_of_edges=10;
+    while(no_of_edges)
+    {
+        char u,v;
+        cin>>u>>v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+        no_of_edges--;
+    }
+    dfs(adjList,'C');
+}
+
