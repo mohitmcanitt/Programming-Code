6f5d0876b3f1f3249a752c64dcf771c7f468b190
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2021-07-22 02:06:52 +0530

    Update 10. Celebrity Problem.cpp

--- Stack/10. Celebrity Problem.cpp
+++ Stack/10. Celebrity Problem.cpp
@@ -85,3 +85,24 @@ int findJudge(int n, vector<vector<int>>& trust) {
         }
         return pot+1;
     }
+
+// Method 3
+
+int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int>indegree(n,0);
+        vector<int>outdegree(n,0);
+        
+        for(int i=0;i<trust.size();i++)
+        {
+            int x=trust[i][0]-1;
+            int y=trust[i][1]-1;
+            indegree[y]++;
+            outdegree[x]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==n-1 and outdegree[i]==0)
+                return i+1;
+        }
+        return -1;
+    }
