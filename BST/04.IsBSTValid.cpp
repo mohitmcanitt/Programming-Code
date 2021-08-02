61b78ea19c4054f4a244058741e7f7a29ea794a9
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2021-08-03 02:41:36 +0530

    Update 04.IsBSTValid.cpp

--- BST/04.IsBSTValid.cpp
+++ BST/04.IsBSTValid.cpp
@@ -1,9 +1,10 @@
-bool isValidBST(node *root,int minV=INT_MIN,int maxV=INT_MAX)
-{
-    if(root==NULL)
-    reutrn true;
-
-    if(root->data>minV and root->data<maxV and isValidBST(root->left,minV,root->data) and isValidBST(root->right,root->data,maxV))
-    return true;
-    return false;
-}
+  bool isBST(Node* root,int low=INT_MIN,int high=INT_MAX) 
+    {
+        // Your code here
+        if(root==NULL)
+            return true;
+        if(root->data<=low or root->data>=high)
+            return false;
+        
+        return isBST(root->left,low,root->data) and isBST(root->right,root->data,high);    
+    }
