6cb1af7e37ce9c81312c72f4c7821893cca886ca
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2021-08-04 23:02:13 +0530

    Update Merge Sort.cpp

--- Sorting/Merge Sort.cpp
+++ Sorting/Merge Sort.cpp
@@ -3,54 +3,47 @@ using namespace std;
 
 void merge(int *arr,int low,int mid,int high)
 {
-	int b[high-low+1];
-	int k=0;
-	int i=low;
-	int j=mid+1;
-	
-	while(i<=mid and j<=high)
-	{
-		if(arr[i]<arr[j])
-			b[k++]=arr[i++];
-		else
-		    b[k++]=arr[j++]	;
-	}
-	if(i>mid)
-		{
-			while(j<=high)
-				b[k++]=arr[j++];
-		}
-	else if(j>high)
-		{
-			while(i<=mid)
-				b[k++]=arr[i++];
-		}	
-	k=0;
-	for(int i=low;i<=high;i++)
-		arr[i]=b[k++];
-		
+    int b[high-low+1];
+    int i=low;
+    int j=mid+1;
+    int k=0;
+    while(i<=mid and j<=high)
+    {
+        if(arr[i]<arr[j])
+            b[k++]=arr[i++];
+        else
+            b[k++]=arr[j++];    
+    }
+    if(i>mid) /// means all element from low to mid is processed
+    {
+        while(j<=high)
+            b[k++]=arr[j++];
+    }
+    if(j>high)
+    {
+        while(i<=mid)
+            b[k++]=arr[i++];
+    }
+    k=0;
+    for(int i=low;i<=high;i++)
+        arr[i]=b[k++];
 }
 
-void merge_sort(int *arr,int low,int high)
+void mergeSort(int *arr,int s,int e)
 {
-	if(low<high)
-		{
-			int mid=(low+high)/2;
-			merge_sort(arr,low,mid);
-			merge_sort(arr,mid+1,high);
-			merge(arr,low,mid,high);
-			
-		}
+    if(s>=e)
+     return;
+    int mid=(s+e)/2;
+    mergeSort(arr,s,mid);
+    mergeSort(arr,mid+1,e);
+
+    merge(arr,s,mid,e);
 }
 
-
-int main()
-{
-	int arr[]={15,4,11,3,2};
-	int n=sizeof(arr)/sizeof(arr[0]);
-	
-	merge_sort(arr,0,n-1);
-	for(int i=0;i<n;i++)
-		cout<<arr[i]<<" ";
-		
+int main() {
+    int arr[5]={5,4,1,3,2};
+    int n=5;
+    mergeSort(arr,0,n-1);
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<" ";
 }
