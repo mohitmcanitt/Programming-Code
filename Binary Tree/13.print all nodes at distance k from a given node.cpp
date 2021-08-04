ece3d413e2a7183290efe95a4de87d2a4f09088b
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2021-08-04 18:12:30 +0530

    Update 13.print all nodes at distance k from a given node.cpp

--- Binary Tree/13.print all nodes at distance k from a given node.cpp
+++ Binary Tree/13.print all nodes at distance k from a given node.cpp
@@ -1,92 +1,51 @@
-/*
-INPUT
-1 2 4 6 -1 -1 7 10 -1 -1 11 -1 -1 5 8 -1 -1 9 -1 -1 3 -1 -1
-OUT{UT
-10 11 5 1
-
-*/
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class node{
-     public:
-    int data;
-    node* left;
-    node *right;
-   
-    node(int d)
+bool find(TreeNode* root,int target,vector<TreeNode*>&path)
     {
-        data=d;
-        left=NULL;
-        right=NULL;
+        if(root==NULL)
+            return false;
+        if(root->val==target)
+        {
+            path.push_back(root);
+            return true;
+        }
+        bool filc=find(root->left,target,path);
+        if(filc)
+       {
+            path.push_back(root);
+            return true;
+        }
+        
+        bool firc=find(root->right,target,path);
+        if(firc)
+        {
+            path.push_back(root);
+            return true;
+        }
+        return false;
     }
-};
-
-node *buildTree()
-{
-    int val;
-    cin>>val;
-    if(val==-1)
-        return NULL;
-    node *root=new node(val);
-    root->left=buildTree();
-    root->right=buildTree();
-    return root;    
-}
-
-vector<node*> pathtokey(node *root,int key)
-{
-    static vector<node*>treenodepath;
-    if(root==NULL)
-    return {};
-    if(root->data==key)
-     {
-        treenodepath.push_back(root);
-        return treenodepath;
-    }
-    // First check in left tree
-    vector<node*>filc=pathtokey(root->left,key);  // filc->find in left child
-    if(filc.size())     // if size is not zero
-     {
-        treenodepath.push_back(root);
-        return treenodepath;
-    }
-    // then check in right tree 
-    vector<node*>firc=pathtokey(root->right,key);  // firc-> find in right child
-    if(firc.size())
+    void kthleveldown(TreeNode *root,TreeNode* blocker,int level,vector<int>&ans)
     {
-        treenodepath.push_back(root);
-        return treenodepath;
+        if(root==NULL or root==blocker or level<0)
+            return;
+        if(level==0)
+        {
+            ans.push_back(root->val);
+            return;
+        }
+        kthleveldown(root->left,blocker,level-1,ans);
+        kthleveldown(root->right,blocker,level-1,ans);
     }
-    else
-    return {};
-}
-
-void printkthlevel(node *src,int k,node *blocker)
-{
-    if(src==NULL or k<0 or blocker==src)
-    return;
-    if(k==0)
-    cout<<src->data<<" ";
-    printkthlevel(src->left,k-1,blocker);
-    printkthlevel(src->right,k-1,blocker);
-}
-
-void print_all_nodes_at_distance_k_from_a_given_node(node *root,int target,int dist)
-{
-    vector<node*>v=pathtokey(root,target); // path(node address) from given node(containing target value) to root node 
-    for(int i=0;i<v.size();i++)
-        printkthlevel(v[i],dist-i,i==0?NULL:v[i-1]);
-
-}
-
-
-int main()
-{
-    node *root=buildTree();
-    print_all_nodes_at_distance_k_from_a_given_node(root,4,3);// print all node at 2 distance from node containing 4.
-    
-    
-}
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
+        vector<int>ans;
+        vector<TreeNode*>path;
+        
+        find(root,target->val,path);
+        
+        for(int i=0;i<path.size();i++)
+        {
+            TreeNode *blocker=NULL;
+            if(i!=0)
+                blocker=path[i-1];
+            kthleveldown(path[i],blocker,K-i,ans);
+        }
+        return ans;
+    }
