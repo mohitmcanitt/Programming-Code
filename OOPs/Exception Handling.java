d336984e818b5ba640e953e635beb181ddaa0030
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2021-10-01 19:51:20 +0530

    Create Exception Handling.java

---
+++ OOPs/Exception Handling.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String args[]) {
+        int a=5;
+        int b=0;
+
+        try
+        {
+            int c=a/b;
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+    }
+}
