f6e5bdfb0254ac8467ae3e539721087e86ccfc67
mohitmcanitt <mohitmcanitt@gmail.com>
2023-10-05 10:15:36 +0530

    Calculate combination

---
+++ Striver/calculate_ncr.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+long calculate_combination(int n, int r){
+    long res=1;
+    for(int i=0;i<r;i++){
+        res=res*(n-i);
+        res=res/(i+1);
+    }
+    return res;
+}
+
+int main(){
+    int n;
+    int r;
+    cout<<"Enter value of n : ";
+    cin>>n;
+    cout<<"Enter value of r : ";
+    cin>>r;
+    long combination= calculate_combination(n,r);
+    cout<<"Combination of given value is : "<<combination<<endl;
+}
