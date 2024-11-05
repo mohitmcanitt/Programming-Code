af0f2cc843b4bb00108db3708358dff413001c61
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2024-11-05 23:36:47 +0530

    Update 06. Next Greater to left.cpp

--- Stack/06. Next Greater to left.cpp
+++ Stack/06. Next Greater to left.cpp
@@ -1,52 +1,46 @@
-#include<bits/stdc++.h>
+/*
+Input  : 4, 15, 2, 14, 12,8
+Output : -1 -1 15 15 14 12 
+*/
+#include <iostream>
+#include <vector>
+#include <stack>
+
 using namespace std;
 
-//  6 8 0 1 3
-//  -1-1 8 8 8
+vector<int> nextGreaterToLeft(const vector<int>& arr) {
+    stack<int> st;
+    vector<int> result;
 
-//  8
+    for (int i = 0; i < arr.size(); i++) {
+        // Pop elements from the stack until we find a greater element or the stack is empty
+        while (!st.empty() && st.top() <= arr[i]) {
+            st.pop();
+        }
 
-void nextGreaterLeft(vector<int>&v)
-{
-    stack<int>st;
-    for(int i=0;i<v.size();i++)
-    {
-        if(st.empty())
-        {
-           st.push(v[i]);
-           v[i]=-1; 
-        }    
-        else if(v[i]<st.top())
-        {
-            int temp=v[i];
-            v[i]=st.top();
-             st.push(temp);
-             
+        // If stack is empty, no greater element to the left
+        if (st.empty()) {
+            result.push_back(-1);
+        } else {
+            result.push_back(st.top());
         }
-        else
-        {
-            while(!st.empty() and st.top()<=v[i])
-                st.pop();
-            if(st.empty())
-            {
-                st.push(v[i]);
-                v[i]=-1;
-            }
-            else
-            {
-                int temp=v[i];
-                v[i]=st.top();
-                st.push(temp);
-            }
-        }
-           
-    }        
+
+        // Push the current element onto the stack
+        st.push(arr[i]);
+    }
+
+    return result;
 }
 
 int main() {
-    vector<int>v={6,8,0,1,3};
-    nextGreaterLeft(v);
-    for(auto x:v)
-        cout<<x<<" ";
+    vector<int> arr = {4, 5, 2, 10, 8};
+    
+    vector<int> result = nextGreaterToLeft(arr);
+    
+    for (int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
 
+    return 0;
 }
