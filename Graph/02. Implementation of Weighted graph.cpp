5f0c40dc64ae8ff23a448bc92a27752c26b993db
Mohit Kumar <72444430+mohitmcanitt@users.noreply.github.com>
2024-11-07 02:27:29 +0530

    Update 02. Implementation of Weighted graph.cpp

--- Graph/02. Implementation of Weighted graph.cpp
+++ Graph/02. Implementation of Weighted graph.cpp
@@ -13,39 +13,44 @@ Neighbours of vertex 2 is --> vertex 3 and their weight is 6,
 #include <bits/stdc++.h>
 using namespace std;
 
-class Graph{
-    int V;
-    map<int,vector<pair<int,int>>>m;  
-    public:
-    Graph(int v)
-    {
-        V=v;
-    }
-    void addEdge(int u,int v,int weight)
-    {
-        m[u].push_back(make_pair(v,weight));
+class Graph {
+    int vertices; // Number of vertices
+    map<int, vector<pair<int, int>>> adjacencyList; // Adjacency list to store graph
+    
+public:
+    // Constructor to initialize number of vertices
+    Graph(int vertices) {
+        this->vertices = vertices;
     }
 
-    void printgraph()
-    {
-        for(auto x:m)
-        {
-            cout<<"Neighbours of vertex "<<x.first<<" is -->";
-            for(auto nbhr:x.second)
-            {
-                cout<<" vertex "<<nbhr.first<<" and their weight is "<<nbhr.second<<",";
+    // Function to add an edge with a weight to the graph
+    void addEdge(int u, int v, int weight) {
+        adjacencyList[u].push_back(make_pair(v, weight)); // Add v and weight to u's adjacency list
+        adjacencyList[v].push_back(make_pair(u, weight)); // Since this is an undirected graph
+    }
+
+    // Function to print the adjacency list representation of the graph
+    void displayGraph() const {
+        for (const auto& vertex : adjacencyList) {
+            cout << "Vertex " << vertex.first << " has neighbors: ";
+            for (const auto& neighbor : vertex.second) {
+                cout << "(" << neighbor.first << ", " << neighbor.second << ") ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
 };
 
 int main() {
-    Graph g(4);
-    g.addEdge(0,1,10);
-    g.addEdge(0,2,3);
-    g.addEdge(0,3,2);
-    g.addEdge(1,3,7);
-    g.addEdge(2,3,6);
-    g.printgraph();
+    Graph g(4); // Create a graph with 4 vertices
+    g.addEdge(0, 1, 10);
+    g.addEdge(0, 2, 3);
+    g.addEdge(0, 3, 2);
+    g.addEdge(1, 3, 7);
+    g.addEdge(2, 3, 6);
+
+    // Display the graph
+    g.displayGraph();
+
+    return 0;
 }
